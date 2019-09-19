fd2c40b2d59f2bcf78d58598727ba46c97c63e89
iroirous <5170763+iroirous@users.noreply.github.com>
2019-09-19 19:06:08 +0900

    リンクを追加した。

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className="App">
       <Calendar />
+      <p id="source-link">
+        ソースコードは<a href="https://github.com/iroirous/ReactSimpleCalendar" target="_blank">Githubリポジトリ</a>で公開しています。<br />
+        トップページは<a href="/">コチラ</a>から。
+      </p>
     </div>
   );
 }
